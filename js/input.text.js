509fe7a302d5b5cf538026fe6d90054a00af21f5
Julian Madrid <julianstivenm8@gmail.com>
2017-04-11 19:40:37 -0500

    upload website

---
+++ js/input.text.js
@@ -0,0 +1,43 @@
+
+jQuery(function($)  
+{
+    $("#contact_form").submit(function()
+    {
+        var email = $("#email").val(); // get email field value
+        var name = $("#name").val(); // get name field value
+        var msg = $("#msg").val(); // get message field value
+        $.ajax(
+        {
+            type: "POST",
+            url: "https://mandrillapp.com/api/1.0/messages/send.json",
+            data: {
+                'key': 'insert api key',
+                'message': {
+                    'from_email': email,
+                    'from_name': name,
+                    'headers': {
+                        'Reply-To': email
+                    },
+                    'subject': 'Website Contact',
+                    'text': msg,
+                    'to': [
+                    {
+                        'email': 'lacasadelturboltda@hotmail.com',
+                        'name': 'Casa del turbo',
+                        'type': 'to'
+                    }]
+                }
+            }
+        })
+        .done(function(response) {
+            alert('Your message has been sent. Thank you!'); // show success message
+            $("#name").val(''); // reset field after successful submission
+            $("#email").val(''); // reset field after successful submission
+            $("#msg").val(''); // reset field after successful submission
+        })
+        .fail(function(response) {
+            alert('Error sending message.');
+        });
+        return false; // prevent page refresh
+    });
+});
